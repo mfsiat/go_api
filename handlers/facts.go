c5c49a35772185dd78ce912407be00c48edaa74d
Md. Nasirul Islam <nasirul.islam@northsouth.edu>
2022-11-15 00:22:52 +0600

    added list route for go (#1)

--- handlers/facts.go
+++ handlers/facts.go
@@ -9,6 +9,15 @@ func Home(c *fiber.Ctx) error {
 	return c.SendString("Hello, Siat")
 }
 
+func ListFacts(c *fiber.Ctx) error {
+	// return c.SendString("Hello, Siat")
+	facts := []models.Fact{}
+
+	database.DB.Db.Find(&facts)
+
+	return c.Status(200).JSON(facts)
+}
+
 func CreateFact(c *fiber.Ctx) error {
 	fact := new(models.Fact)
 	if err := c.BodyParser(fact); err != nil {
